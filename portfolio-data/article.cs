a6e5e19c929ee1fa53cd9ca11f71b4931b914e6a
IyiKuyoro <opeoluwa.iyikuyoro@gmail.com>
2019-03-17 19:18:12 +0100

    add articles class

---
+++ portfolio-data/article.cs
@@ -0,0 +1,11 @@
+namespace portfolio_data
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string Category { get; set; }
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
+        public string Link { get; set; }
+    }
+}
