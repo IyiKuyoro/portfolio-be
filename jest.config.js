a9588c07edab0aa44660e24bcac0a6e5b39dd3da
IyiKuyoro <opeoluwa.iyikuyoro@gmail.com>
2019-04-25 16:24:31 +0200

    feat(user): create super admin
    - add user model and migrations
    - add script to create super admin
    
    [#164072736]

--- jest.config.js
+++ jest.config.js
@@ -4,15 +4,16 @@
 module.exports = {
   automock: false, // All imported modules in your tests should be mocked automatically
   clearMocks: true, // Automatically clear mock calls and instances between every test
-  collectCoverage: true, // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
   collectCoverageFrom: [
-    '<rootDir>/src/**/*.ts',
-    '!<rootDir>/src/index.ts',
-    '!<rootDir>/src/config.ts',
-    '!<rootDir>/src/logger.ts',
+    '<rootDir>/src/**/*.js',
+    '!<rootDir>/src/index.js',
+    '!<rootDir>/src/config.js',
+    '!<rootDir>/src/logger.js',
+    '!<rootDir>/src/scripts/**',
     '!**/node_modules/**',
     '!src/database',
-  ], // An array of glob patterns indicating a set of files for which coverage information should be collected
+  ],
   coverageDirectory: 'coverage', // The directory where Jest should output its coverage files
   coveragePathIgnorePatterns: [
     '<rootDir>/src/database/*',
@@ -36,7 +37,7 @@ module.exports = {
   dependencyExtractor: null, // A path to a custom dependency extractor
   moduleDirectories: [
     'node_modules',
-  ], // An array of directory names to be searched recursively up from the requiring module's location
+  ],
   resetMocks: true, // Automatically reset mock state between every test
   testEnvironment: 'node', // The test environment that will be used for testing
   testMatch: [
@@ -46,10 +47,10 @@ module.exports = {
   ], // The glob patterns Jest uses to detect test files
   testPathIgnorePatterns: [
     '\\\\node_modules\\\\',
-  ], // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
+  ],
   transform: null, // A map from regular expressions to paths to transformers
   transformIgnorePatterns: [
     '\\\\node_modules\\\\',
-  ], // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
+  ],
   verbose: true, // Indicates whether each individual test should be reported during the run
 };
--- src/config.js
+++ src/config.js
@@ -9,16 +9,25 @@ const config = () => {
     case 'test': {
       return {
         PORT: process.env.PORT,
+        SA_EMAIL: process.env.SA_EMAIL,
+        SA_PASSWORD: process.env.SA_PASSWORD,
+        SA_USERNAME: process.env.SA_USERNAME,
       };
     }
-    case 'prod': {
+    case 'production': {
       return {
         PORT: process.env.PORT,
+        SA_EMAIL: process.env.SA_EMAIL,
+        SA_PASSWORD: process.env.SA_PASSWORD,
+        SA_USERNAME: process.env.SA_USERNAME,
       };
     }
     default: {
       return {
         PORT: process.env.PORT,
+        SA_EMAIL: process.env.SA_EMAIL,
+        SA_PASSWORD: process.env.SA_PASSWORD,
+        SA_USERNAME: process.env.SA_USERNAME,
       };
     }
   }
