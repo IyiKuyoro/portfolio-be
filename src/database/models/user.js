66fa2a03d003cc2688eee4d5954d4c8f849dc841
IyiKuyoro <opeoluwa.iyikuyoro@gmail.com>
2019-04-25 16:24:31 +0200

    feat(user): create super admin
    - add user model and migrations
    - add script to create super admin
    
    [#164072736]

---
+++ src/database/models/user.js
@@ -0,0 +1,38 @@
+import bcrypt from 'bcrypt';
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    firstName: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    lastName: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    email: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      set(val) {
+        this.setDataValue('email', val.toLowerCase());
+      },
+      unique: true,
+    },
+    userName: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      unique: true,
+    },
+    password: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      set(val) {
+        this.setDataValue('password', bcrypt.hashSync(val, 10));
+      },
+    },
+  }, {});
+  User.associate = () => {
+    // associations can be defined here
+  };
+  return User;
+};
---
+++ src/scripts/createSuperAdmin.js
@@ -0,0 +1,24 @@
+import models from '../database/models';
+import logger from '../logger';
+import config from '../config';
+
+const { User } = models;
+
+User.findOrCreate({
+  where: {
+    email: config.SA_EMAIL,
+  },
+  defaults: {
+    firstName: 'Opeoluwa',
+    lastName: 'Iyi-Kuyoro',
+    email: config.SA_EMAIL,
+    userName: config.SA_USERNAME,
+    password: config.SA_PASSWORD,
+  },
+})
+  .then(() => {
+    logger.log('info', 'SA created successfully');
+  })
+  .catch((error) => {
+    logger.log('error', `Could not create SA: ${error.message}`);
+  });
