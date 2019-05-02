718935d3fe9ca05d3127c16035324d68c16c46a8
IyiKuyoro <opeoluwa.iyikuyoro@gmail.com>
2019-05-02 17:00:42 +0100

    fix(cors): allow cross origin access
    - add the header to allow cors
    
    [Finishes #165771128]

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ import logger from './logger';
 import router from './routes';
 
 const app = express();
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({
   extended: false,
