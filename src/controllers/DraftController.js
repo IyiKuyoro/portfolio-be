a61300aaaa024b6c5a3a4c8233328eaaed7e04cf
IyiKuyoro <opeoluwa.iyikuyoro@gmail.com>
2019-11-14 16:45:31 +0100

    feat(draft): save draft article

--- src/controllers/DraftController.js
+++ src/controllers/DraftController.js
@@ -1,10 +1,27 @@
 import RespondEx from '@respondex/core';
-import ApiError from '@respondex/apierror';
+
+import DraftService from '../services/DraftService';
 
 export default class DraftController {
   static async createDraft(req, res) {
     try {
-      throw Error('Not Implemented');
+      const draft = await DraftService.upsertDraft({
+        title: req.body.title,
+        authors: [`${req.userData.firstName} ${req.userData.lastName}`],
+        imageUrl: req.body.imageUrl,
+        body: req.body.body,
+      });
+
+      RespondEx.successWithData(
+        'Draft created',
+        {
+          title: draft[0].dataValues.title,
+          authors: draft[0].dataValues.authors,
+          imageUrl: draft[0].dataValues.imageUrl,
+          body: draft[0].dataValues.body,
+        },
+        res,
+      );
     } catch (error) {
       RespondEx.error(error, res);
     }
