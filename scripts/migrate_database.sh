bd6c4048840751c8f673727b5333294e31d4441d
IyiKuyoro <opeoluwa.iyikuyoro@gmail.com>
2019-11-20 14:10:26 +0100

    chore(setup): add circleci config
    - add migration script
    - add deployment script
    - add login script

---
+++ scripts/migrate_database.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script will run the database migrations against the staging and production databases
+
+# Check for the branch name and set the right app name
+if [ "${CIRCLE_BRANCH}" = "master" ]; then
+  export HEROKU_APP_NAME='iyikuyoro-be'
+elif [ "${CIRCLE_BRANCH}" = "develop" ]; then
+  export HEROKU_APP_NAME='iyikuyoro-staging'
+fi
+
+# Login to heroku, pull the database URL and run migrations
+chmod u+x scripts/heroku_login.sh
+scripts/heroku_login.sh
+export DATABASE_URL=$(heroku config:get DATABASE_URL -a ${HEROKU_APP_NAME})
+
+NODE_ENV=production npm run migrate
