2ec6633e0a590758c88b198a298c133c5a454d6e
IyiKuyoro <opeoluwa.iyikuyoro@gmail.com>
2019-12-01 16:14:31 +0100

    setup data seeders

---
+++ src/database/seeders/20191201132301-test-user.js
@@ -0,0 +1,17 @@
+const bcrypt = require('bcrypt');
+
+module.exports = {
+  up: queryInterface => queryInterface.bulkInsert('Users', [{
+    id: 1,
+    firstName: 'Test',
+    lastName: 'User',
+    userName: 'testuser',
+    password: bcrypt.hashSync('password', 10),
+    email: 'user@test.com',
+    role: 'super-admin',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  }], {}),
+
+  down: queryInterface => queryInterface.bulkDelete('Users', null, {}),
+};
